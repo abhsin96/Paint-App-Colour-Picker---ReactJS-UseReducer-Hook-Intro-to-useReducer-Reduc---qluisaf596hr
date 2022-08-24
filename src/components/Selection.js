c03781c8b9bf7fb70b60c44176915c45874fefed
abhsin96 <abhsin96@gmail.com>
2022-08-24 23:18:21 +0530

    first commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
+import React, { useState } from "react";
 
+function Selection({nextBackground,applyColor}){
+    const [color, setColor] = useState({})
+
+    return <div className="ifx-box" style={color} onClick={()=>setColor(nextBackground)}>
+        <h2 className="subheading">Selection</h2>
+    </div>
+}
+
+export default Selection
