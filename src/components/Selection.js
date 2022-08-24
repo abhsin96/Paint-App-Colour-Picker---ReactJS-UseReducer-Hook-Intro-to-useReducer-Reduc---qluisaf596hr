d547006792040e706634450af1571af0e3ace00e
abhsin96 <abhsin96@gmail.com>
2022-08-24 23:37:34 +0530

    initial state

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 function Selection({applyColor}){
-    const [color, setColor] = useState({})
+    const [color, setColor] = useState({background:''})
     console.log(color)
-    return <div className="ifx-box" style={color || {background: ""}} onClick={()=>applyColor(setColor)}>
+    return <div className="ifx-box" style={color} onClick={()=>applyColor(setColor)}>
         <h2 className="subheading">Selection</h2>
     </div>
 }
