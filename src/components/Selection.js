71ba63a8b40a04efe84a9978ee5cf2c96ce99b2a
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2022-08-26 12:07:35 +0530

    name change
    name change

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 function Selection({applyColor}){
-    const [color, setColor] = useState({background:''})
+    const [state, setState] = useState({background:''})
     console.log(color)
-    return <div className="ifx-box" style={color} onClick={()=>applyColor(setColor)}>
+    return <div className="ifx-box" style={state} onClick={()=>applyColor(setState)}>
         <h2 className="subheading">Selection</h2>
     </div>
 }
